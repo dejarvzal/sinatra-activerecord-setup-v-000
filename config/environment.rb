3c9579e3428fed00ce443f849b4ca5c32bdc0d05
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2019-08-31 15:15:10 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,6 @@ ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
-ruby
 configure :development do
   set :database, 'sqlite3:db/database.db'
 end
